5467e2993afde583061761b378b8c78bed2b24f5
Felix <jonfelixrico@gmail.com>
2021-09-04 14:22:51 +0800

    Generate submodule for discord interactions

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { discordProviders } from './discord.providers'
+import { InteractionsModule } from './interactions/interactions.module'
 
 @Module({
   providers: [...discordProviders],
+  imports: [InteractionsModule],
 })
 export class DiscordModule {}
