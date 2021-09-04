fe3109d9146ec5398b0e6be770317918a7734f99
Felix <jonfelixrico@gmail.com>
2021-09-04 15:38:29 +0800

    Push code for command registration

--- src/discord/interactions/services/command-registration/command-registration.service.ts
+++ src/discord/interactions/services/command-registration/command-registration.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Client } from 'discord.js'
+import { REST } from '@discordjs/rest'
+import { ROLL_COMMAND } from './builders/roll.command-builder'
+import { Routes } from 'discord-api-types'
+
+const COMMAND_BUILDERS = [ROLL_COMMAND]
 
 @Injectable()
-export class CommandRegistrationService {}
+export class CommandRegistrationService implements OnApplicationBootstrap {
+  constructor(private client: Client, private config: ConfigService) {}
+
+  get registrationRoute(): `/${string}` {
+    const { client, config } = this
+    const commandGuildId: string = config.get('COMMAND_GUILD_ID')
+
+    if (commandGuildId) {
+      return Routes.applicationGuildCommands(
+        client.application.id,
+        commandGuildId
+      )
+    }
+
+    return Routes.applicationCommands(client.application.id)
+  }
+
+  async onApplicationBootstrap() {
+    const { client, registrationRoute } = this
+    const rest = new REST({ version: '9' }).setToken(client.token)
+
+    await rest.put(registrationRoute, {
+      body: COMMAND_BUILDERS.map((command) => command.toJSON()),
+    })
+  }
+}
