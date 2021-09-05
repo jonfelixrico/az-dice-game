b3bbc2b1b07847d5238c6aee6c40ca2350a3094c
Felix <jonfelixrico@gmail.com>
2021-09-06 00:32:29 +0800

    Apply minor changes to the GameSessionRepositoryService

--- src/write-model/game-session-repository/game-session-repository.service.ts
+++ src/write-model/game-session-repository/game-session-repository.service.ts
@@ -2,13 +2,14 @@ import { EventStoreDBClient, JSONEventType } from '@eventstore/db-client'
 import { Injectable } from '@nestjs/common'
 import { GameSession } from 'src/domain/entities/game-session.entity'
 import { IUserRoll } from 'src/domain/entities/user-roll.interface'
+import { EventName } from 'src/domain/events/base-domain-event.interface'
 
-export interface IPartialUserRoll extends Record<string | number, unknown> {
+interface IBaseUserRoll extends Record<string | number, unknown> {
   rollId: string
   userId: string
 }
 
-interface EsdbUserRoll extends IUserRoll, Record<string, unknown> {}
+interface EsdbUserRoll extends IUserRoll, IBaseUserRoll {}
 
 @Injectable()
 export class GameSessionRepositoryService {
@@ -24,51 +25,41 @@ export class GameSessionRepositoryService {
     let lastRoll: IUserRoll
     let lastRev: bigint
 
-    const resolvedEvts = this.client.readStream<
-      JSONEventType<string, IPartialUserRoll | EsdbUserRoll>
+    const stream = this.client.readStream<
+      JSONEventType<EventName, IBaseUserRoll>
     >([guildId, channelId].join('/'), {
       direction: 'backwards',
-      // TODO implement full pagination
-      maxCount: 1000,
       fromRevision: 'end',
     })
 
-    for await (const { event } of resolvedEvts) {
+    for await (const { event } of stream) {
       const { type, data, revision } = event
+
       if (lastRev ?? true) {
         lastRev = revision
       }
 
-      const { rollId } = data
-
-      switch (type) {
-        case 'ROLL_REMOVED': {
-          if (restorePool.has(rollId)) {
-            restorePool.delete(rollId)
-            break
-          }
-
-          removePool.add(rollId)
-          break
-        }
-
-        case 'ROLL_RESTORED': {
-          restorePool.add(rollId)
-          break
-        }
-
-        case 'ROLL_CREATED': {
-          if (removePool.has(rollId)) {
-            break
-          }
-
-          lastRoll = data as EsdbUserRoll
-          break
-        }
-      }
-
       if (lastRoll) {
         break
+      }
+
+      const { rollId } = data
+
+      if (type === 'ROLL_REMOVED') {
+        if (restorePool.has(rollId)) {
+          restorePool.delete(rollId)
+          continue
+        }
+
+        removePool.add(rollId)
+      } else if (type === 'ROLL_RESTORED') {
+        restorePool.add(rollId)
+      } else if (type === 'ROLL_CREATED') {
+        if (removePool.has(rollId)) {
+          continue
+        }
+
+        lastRoll = data as EsdbUserRoll
       }
     }
 
