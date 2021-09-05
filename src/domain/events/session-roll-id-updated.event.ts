5fa9856654e3b40c290033d1ceae31c0b116e36a
Felix <jonfelixrico@gmail.com>
2021-09-06 01:10:07 +0800

    Create event for setting the session roll id

---
+++ src/domain/events/session-roll-id-updated.event.ts
@@ -0,0 +1,19 @@
+import { BaseDomainEvent } from './base-domain-event.interface'
+
+export interface ISessionRollIdUpdatedEventPayload {
+  channelId: string
+  guildId: string
+  rollId: string
+}
+
+export class RollCreatedEvent extends BaseDomainEvent<ISessionRollIdUpdatedEventPayload> {
+  constructor(payload: ISessionRollIdUpdatedEventPayload) {
+    const { channelId, guildId } = payload
+
+    super({
+      entityId: [guildId, channelId].join('/'),
+      eventName: 'SESSION_ROLL_ID_UPDATED',
+      payload,
+    })
+  }
+}
