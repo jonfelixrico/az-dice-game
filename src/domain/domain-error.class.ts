832864aa9738097c966c3f25b7257dfacedb4baa
Felix <jonfelixrico@gmail.com>
2021-09-06 00:50:12 +0800

    Create new error codes

--- src/domain/domain-error.class.ts
+++ src/domain/domain-error.class.ts
@@ -1,9 +1,12 @@
 enum DomainErrorCodes {
   USER_WAS_LAST_ROLL = 'USER_WAS_LAST_ROLL',
+  ROLL_NOT_FOUND = 'ROLL_NOT_FOUND',
+  ROLL_ALREADY_REMOVED = 'ROLL_ALREADY_REMOVED',
+  ROLL_NOT_RESTORABLE_BECAUSE_NOT_REMOVED = 'ROLL_NOT_RESTORABLE_BECAUSE_NOT_REMOVED',
 }
 
 export class DomainError extends Error {
-  constructor(code: keyof typeof DomainErrorCodes) {
+  constructor(code: keyof typeof DomainErrorCodes | DomainErrorCodes) {
     super(code)
   }
 }
