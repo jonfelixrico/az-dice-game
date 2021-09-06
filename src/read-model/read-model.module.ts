3dc0ca0e959f8f0dc252e6a9a4fe394aa71c5694
Felix <jonfelixrico@gmail.com>
2021-09-07 01:34:50 +0800

    Import the write model module into the read model module

--- src/read-model/read-model.module.ts
+++ src/read-model/read-model.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { WriteModelModule } from 'src/write-model/write-model.module'
 import { typeormProviders } from './typeorm.providers'
 
 @Module({
+  imports: [WriteModelModule],
   providers: [...typeormProviders],
 })
 export class ReadModelModule {}
