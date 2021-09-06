2d866af4474943a83d4a75ba5f19870bcc13fedf
Felix <jonfelixrico@gmail.com>
2021-09-07 00:51:53 +0800

    Remove unused write repositories

--- src/write-model/write-model.module.ts
+++ src/write-model/write-model.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common'
 import { esdbProviders } from './esdb.providers'
-import { GameSessionRepositoryService } from './game-session-repository/game-session-repository.service'
 
 @Module({
-  providers: [...esdbProviders, GameSessionRepositoryService],
+  providers: [...esdbProviders],
 })
 export class WriteModelModule {}
