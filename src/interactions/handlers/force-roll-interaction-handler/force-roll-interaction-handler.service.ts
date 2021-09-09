a334db7e95bb7fd0886aeaffe9e1fd51d8ad9e8a
Felix <jonfelixrico@gmail.com>
2021-09-09 20:53:02 +0800

    Create handler for /forceroll

---
+++ src/interactions/handlers/force-roll-interaction-handler/force-roll-interaction-handler.service.ts
@@ -0,0 +1,20 @@
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
+
+@EventsHandler(InteractionCreatedEvent)
+export class ForceRollInteractionHandlerService
+  implements IEventHandler<InteractionCreatedEvent>
+{
+  async handle({ interaction }: InteractionCreatedEvent) {
+    if (!interaction.isCommand() || interaction.commandName !== 'roll') {
+      return
+    }
+
+    await interaction.deferReply()
+
+    // TODO push roll event here
+
+    // TODO implement rolling
+    await interaction.editReply('placeholder')
+  }
+}
--- src/interactions/interactions.module.ts
+++ src/interactions/interactions.module.ts
@@ -5,12 +5,14 @@ import { ReadModelModule } from 'src/read-model/read-model.module'
 import { RollInteractionHandlerService } from './handlers/roll-interaction-handler/roll-interaction-handler.service'
 import { CommandRegistrationService } from './services/command-registration/command-registration.service'
 import { InteractionEventsRelayService } from './services/interaction-events-relay/interaction-events-relay.service'
+import { ForceRollInteractionHandlerService } from './handlers/force-roll-interaction-handler/force-roll-interaction-handler.service'
 
 @Module({
   providers: [
     RollInteractionHandlerService,
     CommandRegistrationService,
     InteractionEventsRelayService,
+    ForceRollInteractionHandlerService,
   ],
   imports: [DiscordModule, ReadModelModule, EventStoreDBClient],
 })
