eb93554e600509bfddcb291c42d338e2549dbc2a
Felix <jonfelixrico@gmail.com>
2021-09-09 21:16:52 +0800

    Apply minor changes to RollCreatedEventInterface

--- src/write-model/types/roll-created-event.interface.ts
+++ src/write-model/types/roll-created-event.interface.ts
@@ -1,17 +1,12 @@
 import { IBaseEvent, IBaseEventPayload } from './base-event.interface'
 
-export enum IRollTypes {
-  NATURAL = 'NATURAL',
-  NATURAL_FORCED_TURN = 'NATURAL_FORCED_TURN',
-  MANUAL = 'MANUAL',
-  PROXY = 'PROXY',
-}
+type RollType = 'NATURAL' | 'NATURAL_FORCED_TURN' | 'MANUAL' | 'PROXY'
 
 export interface IRollCreatedEventPayload extends IBaseEventPayload {
   rollId: string
   roll: number[]
 
-  type: keyof typeof IRollTypes | IRollTypes
+  type: RollType
   rollDt: Date
 
   rolledBy: string
