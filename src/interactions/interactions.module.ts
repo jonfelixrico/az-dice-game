cd2074e1feacfb14099da37db2846ef322f9378f
Felix <jonfelixrico@gmail.com>
2021-09-09 20:39:59 +0800

    Integrate ReadModelModule and WriteModelModule into InteractionsModule

--- src/interactions/interactions.module.ts
+++ src/interactions/interactions.module.ts
@@ -1,5 +1,7 @@
+import { EventStoreDBClient } from '@eventstore/db-client'
 import { Module } from '@nestjs/common'
 import { DiscordModule } from 'src/discord/discord.module'
+import { ReadModelModule } from 'src/read-model/read-model.module'
 import { RollInteractionHandlerService } from './handlers/roll-interaction-handler/roll-interaction-handler.service'
 import { CommandRegistrationService } from './services/command-registration/command-registration.service'
 import { InteractionEventsRelayService } from './services/interaction-events-relay/interaction-events-relay.service'
@@ -10,6 +12,6 @@ import { InteractionEventsRelayService } from './services/interaction-events-rel
     CommandRegistrationService,
     InteractionEventsRelayService,
   ],
-  imports: [DiscordModule],
+  imports: [DiscordModule, ReadModelModule, EventStoreDBClient],
 })
 export class InteractionsModule {}
