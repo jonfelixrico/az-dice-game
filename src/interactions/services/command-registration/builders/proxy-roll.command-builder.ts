7ea47e0a486e912c3bf74aa08863a8a9a20cbe07
Felix <jonfelixrico@gmail.com>
2021-09-09 20:56:45 +0800

    Make the user option required

--- src/interactions/services/command-registration/builders/proxy-roll.command-builder.ts
+++ src/interactions/services/command-registration/builders/proxy-roll.command-builder.ts
@@ -4,5 +4,8 @@ export const PROXY_ROLL_COMMAND = new SlashCommandBuilder()
   .setName('proxyroll')
   .setDescription('Roll the dice for another user.')
   .addUserOption((option) =>
-    option.setName('user').setDescription('The user you will roll for.')
+    option
+      .setName('user')
+      .setDescription('The user you will roll for.')
+      .setRequired(true)
   )
