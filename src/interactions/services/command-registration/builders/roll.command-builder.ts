4f67d1584baca9dce6b80916df2b69f1053a23f0
Felix <jonfelixrico@gmail.com>
2021-09-09 20:51:12 +0800

    Remove the `forceturn` option from the /roll command definition

--- src/interactions/services/command-registration/builders/roll.command-builder.ts
+++ src/interactions/services/command-registration/builders/roll.command-builder.ts
@@ -3,9 +3,3 @@ import { SlashCommandBuilder } from '@discordjs/builders'
 export const ROLL_COMMAND = new SlashCommandBuilder()
   .setName('roll')
   .setDescription('Roll the dice.')
-  .addBooleanOption((opt) =>
-    opt
-      .setName('forceturn')
-      //  TODO make description more clear; explain turn system
-      .setDescription("Force the roll even if it isn't your turn yet")
-  )
