d680e1d59c4cca0c0b21debbecad4ddd69052930
Felix <jonfelixrico@gmail.com>
2021-09-09 21:08:27 +0800

    Push some placeholder code into the /proxyroll command handler

--- src/interactions/handlers/proxy-roll-interaction-handler/proxy-roll-interaction-handler.service.ts
+++ src/interactions/handlers/proxy-roll-interaction-handler/proxy-roll-interaction-handler.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common'
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
 
-@Injectable()
-export class ProxyRollInteractionHandlerService {}
+@EventsHandler(InteractionCreatedEvent)
+export class ProxyRollInteractionHandlerService
+  implements IEventHandler<InteractionCreatedEvent>
+{
+  async handle({ interaction }: InteractionCreatedEvent) {
+    if (!interaction.isCommand() || interaction.commandName !== 'proxyroll') {
+      return
+    }
+
+    const rollOwner = interaction.options.getUser('user')
+
+    if (interaction.user.equals(rollOwner)) {
+      await interaction.reply({
+        ephemeral: true,
+        content:
+          'You assigned yourself as the user. Please use /roll or /forceroll instead.',
+      })
+      return
+    }
+
+    await interaction.deferReply()
+
+    // TODO send roll event
+
+    await interaction.editReply('placeholder')
+  }
+}
