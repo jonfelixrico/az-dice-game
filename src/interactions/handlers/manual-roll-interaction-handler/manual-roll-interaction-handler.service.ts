c7b6178415f86170d6a8905d7b8547bddc83b1f2
Felix <jonfelixrico@gmail.com>
2021-09-09 21:13:19 +0800

    Push some logic for the /manualroll handler

--- src/interactions/handlers/manual-roll-interaction-handler/manual-roll-interaction-handler.service.ts
+++ src/interactions/handlers/manual-roll-interaction-handler/manual-roll-interaction-handler.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common'
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
 
-@Injectable()
-export class ManualRollInteractionHandlerService {}
+const ROLL_STRING_REGEX = /^[123456]{6}$/
+
+@EventsHandler(InteractionCreatedEvent)
+export class ManualRollInteractionHandlerService
+  implements IEventHandler<InteractionCreatedEvent>
+{
+  async handle({ interaction }: InteractionCreatedEvent) {
+    if (!interaction.isCommand() || interaction.commandName !== 'manualroll') {
+      return
+    }
+
+    const { options } = interaction
+
+    const rollOwner = options.getUser('user') ?? interaction.user
+    const rawRollString = options.getString('rollstring')
+
+    if (ROLL_STRING_REGEX.test(rawRollString)) {
+      await interaction.reply({
+        ephemeral: true,
+        content:
+          'Please make sure that there are 6 characters in the string, and each character has a value between 1 and 6.',
+      })
+      return
+    }
+
+    await interaction.deferReply()
+
+    // TODO send roll event
+
+    await interaction.editReply('placeholder')
+  }
+}
