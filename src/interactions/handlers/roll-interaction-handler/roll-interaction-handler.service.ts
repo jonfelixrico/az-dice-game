a2fe3877a42305855c1fedcff3bf55e7b6b74da8
Felix <jonfelixrico@gmail.com>
2021-09-11 16:15:37 +0800

    Integrate RollEventHelper into RollInteractionHandler

--- src/interactions/handlers/roll-interaction-handler/roll-interaction-handler.service.ts
+++ src/interactions/handlers/roll-interaction-handler/roll-interaction-handler.service.ts
@@ -1,12 +1,17 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
 import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
+import { RollEventHelperService } from 'src/interactions/services/roll-event-helper/roll-event-helper.service'
 import { RollDbEntity } from 'src/read-model/entities/roll.db-entity'
 import { Connection, IsNull, Not } from 'typeorm'
+
 @EventsHandler(InteractionCreatedEvent)
 export class RollInteractionHandlerService
   implements IEventHandler<InteractionCreatedEvent>
 {
-  constructor(private typeorm: Connection) {}
+  constructor(
+    private typeorm: Connection,
+    private rollHelper: RollEventHelperService
+  ) {}
 
   async handle({ interaction }: InteractionCreatedEvent) {
     if (!interaction.isCommand() || interaction.commandName !== 'roll') {
@@ -28,9 +33,12 @@ export class RollInteractionHandlerService
       )
     }
 
-    // TODO push roll event here
+    const rolled = await this.rollHelper.createRoll({
+      interaction,
+      type: 'NATURAL',
+    })
 
-    // TODO implement rolling
-    await interaction.editReply('placeholder')
+    // TODO create formatter for this
+    await interaction.editReply(JSON.stringify(rolled.roll))
   }
 }
