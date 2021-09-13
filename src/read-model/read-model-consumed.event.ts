e0bfa2afb1330a93053e108711f418d484b0d1bd
Felix <jonfelixrico@gmail.com>
2021-09-13 21:18:11 +0800

    Add comment explaining what ReadModelConsumedEvent means

--- src/read-model/read-model-consumed.event.ts
+++ src/read-model/read-model-consumed.event.ts
@@ -1,6 +1,9 @@
-import { JSONRecordedEvent } from '@eventstore/db-client'
+import { AllStreamRecordedEvent } from '@eventstore/db-client'
 import { IEvent } from '@nestjs/cqrs'
 
+/**
+ * Represents the event where the read model has successfully consumed an event from the ESDB stream.
+ */
 export class ReadModelConsumedEvent implements IEvent {
-  constructor(readonly event: JSONRecordedEvent) {}
+  constructor(readonly event: AllStreamRecordedEvent) {}
 }
