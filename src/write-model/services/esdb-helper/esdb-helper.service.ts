4762488115e1d53582f6ea8ea8e4953c8bf57495
Felix <jonfelixrico@gmail.com>
2021-09-13 21:24:48 +0800

    Create WriteModelPublishedEvent

--- src/write-model/services/esdb-helper/esdb-helper.service.ts
+++ src/write-model/services/esdb-helper/esdb-helper.service.ts
@@ -1,20 +1,28 @@
 import { EventStoreDBClient, jsonEvent } from '@eventstore/db-client'
 import { Injectable } from '@nestjs/common'
+import { EventBus } from '@nestjs/cqrs'
+import { WriteModelPublishedEvent } from 'src/write-model/write-model-published.event'
 import { IBaseEvent } from '../../types/base-event.interface'
 
 @Injectable()
 export class EsdbHelperService {
-  constructor(private client: EventStoreDBClient) {}
+  constructor(private client: EventStoreDBClient, private eventBus: EventBus) {}
 
   async pushEvent<E extends IBaseEvent>(event: E): Promise<E> {
     const { payload, type } = event
     const { guildId, channelId } = payload
 
-    await this.client.appendToStream(
-      `${guildId}/${channelId}`,
-      jsonEvent({
-        type: type,
-        data: payload,
+    const esdbEvent = jsonEvent({
+      type: type,
+      data: payload,
+    })
+
+    await this.client.appendToStream(`${guildId}/${channelId}`, esdbEvent)
+
+    this.eventBus.publish(
+      new WriteModelPublishedEvent({
+        event,
+        eventId: esdbEvent.id,
       })
     )
 
