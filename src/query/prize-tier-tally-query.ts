464b46b51c26b18b12fb9cb722cec7f3574b653c
Felix <jonfelixrico@gmail.com>
2021-09-14 21:57:54 +0800

    Expose the typedef of PrizeTierTallyQueryOutput members

--- src/query/prize-tier-tally-query.ts
+++ src/query/prize-tier-tally-query.ts
@@ -3,14 +3,16 @@ import { IQuery } from '@nestjs/cqrs'
 export interface PrizeTierTallyQueryInput {
   guildId: string
   channelId: string
-  startingTime: Date
+  startingTime?: Date
 }
 
-export type PrizeTierTallyQueryOutput = {
+export interface PrizeTierTallyEntry {
   rank: number
   subrank?: number
   count: number
-}[]
+}
+
+export type PrizeTierTallyQueryOutput = PrizeTierTallyEntry[]
 
 export class PrizeTierTallyQuery implements IQuery {
   constructor(readonly input: PrizeTierTallyQueryInput) {}
