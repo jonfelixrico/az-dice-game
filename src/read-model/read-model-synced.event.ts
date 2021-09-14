f98d3bb5301ea87d2e5ac1dbfc3552469286e6b8
Felix <jonfelixrico@gmail.com>
2021-09-14 19:41:06 +0800

    Revert "Adjust the structure of ReadModelSyncedEvent"
    This reverts commit dafa944537edbac91fcc03f10b415d45eff0bd5c.

--- src/read-model/read-model-synced.event.ts
+++ src/read-model/read-model-synced.event.ts
@@ -1,24 +1,11 @@
 import { IEvent } from '@nestjs/cqrs'
-import {
-  IBaseEvent,
-  IBaseEventPayload,
-} from 'src/write-model/types/base-event.interface'
+import { IBaseEvent } from 'src/write-model/types/base-event.interface'
 
 /**
  * Represents a time where we the read model has consumed an event that we also published by the same app instance.
  */
 export class ReadModelSyncedEvent<E extends IBaseEvent = IBaseEvent>
-  implements IEvent, IBaseEvent
+  implements IEvent
 {
-  constructor({ type, payload }: E) {
-    this.type = type
-    this.payload = payload
-  }
-
-  type:
-    | 'ROLL_CREATED'
-    | 'ROLL_REMOVED'
-    | 'ROLL_RESTORED'
-    | 'CHANNEL_SESSION_ROLL_ID_SET'
-  payload: IBaseEventPayload
+  constructor(readonly payload: E) {}
 }
