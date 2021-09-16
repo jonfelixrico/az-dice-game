34d4280e20fba2c608693a76b3c9347e391a990b
Felix <jonfelixrico@gmail.com>
2021-09-16 23:08:19 +0800

    Create handler for LastRollQuery

---
+++ src/query/handlers/last-roll-query-handler/last-roll-query-handler.service.ts
@@ -0,0 +1,49 @@
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs'
+import { LastRollQuery, LastRollQueryOutput } from 'src/query/last-roll.query'
+import { RollDbEntity } from 'src/read-model/entities/roll.db-entity'
+import { Connection, FindConditions, IsNull, MoreThan } from 'typeorm'
+
+@QueryHandler(LastRollQuery)
+export class LastRollQueryHandlerService
+  implements IQueryHandler<LastRollQuery>
+{
+  constructor(private typeorm: Connection) {}
+
+  async execute({ input }: LastRollQuery): Promise<LastRollQueryOutput> {
+    const { channelId, guildId, startingFrom } = input
+
+    const where: FindConditions<RollDbEntity> = {
+      channelId,
+      guildId,
+      deleteDt: IsNull(),
+    }
+
+    if (startingFrom) {
+      where.timestamp = MoreThan(startingFrom)
+    }
+
+    const lastRoll = await this.typeorm.getRepository(RollDbEntity).findOne({})
+
+    if (!lastRoll) {
+      return null
+    }
+
+    const {
+      roll,
+      rollOwner,
+      rolledBy,
+      prizeRank: rank,
+      prizeSubrank: subrank,
+      timestamp,
+    } = lastRoll
+
+    return {
+      roll,
+      rollOwner,
+      rolledBy,
+      rank,
+      subrank,
+      timestamp,
+    }
+  }
+}
--- src/query/query.module.ts
+++ src/query/query.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common'
 import { ReadModelModule } from 'src/read-model/read-model.module'
 import { PrizeTierTallyQueryHandlerService } from './handlers/prize-tier-tally-query-handler/prize-tier-tally-query-handler.service'
 import { ChannelCutoffTimestampQueryHandlerService } from './handlers/channel-cutoff-timestamp-query-handler/channel-cutoff-timestamp-query-handler.service'
+import { LastRollQueryHandlerService } from './handlers/last-roll-query-handler/last-roll-query-handler.service'
 
 @Module({
   providers: [
     PrizeTierTallyQueryHandlerService,
     ChannelCutoffTimestampQueryHandlerService,
+    LastRollQueryHandlerService,
   ],
   imports: [ReadModelModule],
 })
