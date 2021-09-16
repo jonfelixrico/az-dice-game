c9352534855528c4b2b4ca2a09a9a5a98fd76618
Felix <jonfelixrico@gmail.com>
2021-09-16 22:03:49 +0800

    Push logic for HistoryTallyInteractionHandler

--- src/interactions/handlers/history-tally-interaction-handler/history-tally-interaction-handler.service.ts
+++ src/interactions/handlers/history-tally-interaction-handler/history-tally-interaction-handler.service.ts
@@ -1,4 +1,57 @@
-import { Injectable } from '@nestjs/common'
+import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs'
+import { MessageEmbedOptions } from 'discord.js'
+import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
+import {
+  PrizeTierTallyQuery,
+  PrizeTierTallyQueryOutput,
+} from 'src/query/prize-tier-tally-query'
+import { PRIZE_TIERS } from 'src/utils/prize-tier'
 
-@Injectable()
-export class HistoryTallyInteractionHandlerService {}
+@EventsHandler(InteractionCreatedEvent)
+export class HistoryTallyInteractionHandlerService
+  implements IEventHandler<InteractionCreatedEvent>
+{
+  constructor(private queryBus: QueryBus) {}
+
+  async handle({ interaction }: InteractionCreatedEvent) {
+    const { guildId, channelId } = interaction
+
+    if (
+      !interaction.isCommand() ||
+      interaction.commandName === 'history' ||
+      interaction.options.getSubcommand() !== 'tally'
+    ) {
+      return
+    }
+
+    await interaction.deferReply()
+
+    const tally: PrizeTierTallyQueryOutput = await this.queryBus.execute(
+      new PrizeTierTallyQuery({ guildId, channelId })
+    )
+
+    const prizeTiers = PRIZE_TIERS.map(({ name, subrank, rank }) => {
+      const tallyEntry = tally.find(
+        (tally) => tally.rank === rank && tally.subrank === subrank
+      )
+
+      return {
+        name,
+        count: tallyEntry?.count ?? 0,
+      }
+    })
+
+    const embed: MessageEmbedOptions = {
+      author: {
+        name: 'Prize Tally',
+      },
+      description: prizeTiers
+        .map(({ name, count }) => `**${name}** - **${count}** matching rolls`)
+        .join('\n'),
+    }
+
+    await interaction.editReply({
+      embeds: [embed],
+    })
+  }
+}
