27ab51f49c64817a90e680498d472c3ffb98ae5f
Felix <jonfelixrico@gmail.com>
2021-09-16 22:44:45 +0800

    Re-integrate the use of ConfigService in TypeormProvider

--- src/read-model/typeorm.provider.ts
+++ src/read-model/typeorm.provider.ts
@@ -1,4 +1,5 @@
 import { Provider } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Connection, createConnection } from 'typeorm'
 import { ChannelDbEntity } from './entities/channel.db-entity'
 import { EntryDbEntity } from './entities/entry.db-entity'
@@ -6,11 +7,13 @@ import { RollDbEntity } from './entities/roll.db-entity'
 
 export const typeormProvider: Provider = {
   provide: Connection,
-  useFactory: () =>
-    // we'll get our url from the TYPEORM_URL env var, handled by typeorm internally
+  inject: [ConfigService],
+  useFactory: (cfg: ConfigService) =>
     createConnection({
       type: 'postgres',
       database: 'readmodel',
       entities: [RollDbEntity, EntryDbEntity, ChannelDbEntity],
+      url: cfg.get('TYPEORM_URL'),
+      synchronize: !!cfg.get('TYPEORM_SYNCHRONIZE'),
     }),
 }
