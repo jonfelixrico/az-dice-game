cd436a2e67a8053cc438eef9c7310f9924ae06d0
Felix <jonfelixrico@gmail.com>
2021-09-16 22:08:36 +0800

    Push implementation for HIstoryClearInteractionHandlerService

--- src/interactions/handlers/history-clear-interaction-handler/history-clear-interaction-handler.service.ts
+++ src/interactions/handlers/history-clear-interaction-handler/history-clear-interaction-handler.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common'
+import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { MessageEmbedOptions } from 'discord.js'
+import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
+import { EsdbHelperService } from 'src/write-model/services/esdb-helper/esdb-helper.service'
+import { IChannelCutoffTimestampSetEvent } from 'src/write-model/types/channel-cutoff-timestamp-set.event'
 
-@Injectable()
-export class HistoryClearInteractionHandlerService {}
+@EventsHandler(InteractionCreatedEvent)
+export class HistoryClearInteractionHandlerService
+  implements IEventHandler<InteractionCreatedEvent>
+{
+  constructor(private helper: EsdbHelperService) {}
+
+  async handle({ interaction }: InteractionCreatedEvent) {
+    if (
+      !interaction.isCommand() ||
+      interaction.commandName === 'history' ||
+      interaction.options.getSubcommand() !== 'clear'
+    ) {
+      return
+    }
+
+    await interaction.deferReply()
+
+    const { channelId, guildId, user } = interaction
+
+    await this.helper.pushEvent<IChannelCutoffTimestampSetEvent>({
+      type: 'CHANNEL_CUTOFF_TIMESTAMP_SET',
+      payload: {
+        channelId,
+        guildId,
+        cutoffTimestamp: new Date(),
+        timestamp: new Date(),
+        userId: user.id,
+      },
+    })
+
+    const embed: MessageEmbedOptions = {
+      author: {
+        name: 'History Cleared',
+      },
+      description: `${user} has cleared the roll history for this channel.`,
+    }
+
+    await interaction.editReply({
+      embeds: [embed],
+    })
+  }
+}
