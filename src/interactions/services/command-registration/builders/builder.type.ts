54ae34781bccaa02f5233b47a169a9e1692a4da0
Felix <jonfelixrico@gmail.com>
2021-09-18 02:26:23 +0800

    Revert "Add optional `type` to CommandBuilderOutput"
    This reverts commit c0c6aa29756259f73dc7af1adc9eb354cf1db451.

--- src/interactions/services/command-registration/builders/builder.type.ts
+++ src/interactions/services/command-registration/builders/builder.type.ts
@@ -1,7 +1,3 @@
 import { SlashCommandBuilder } from '@discordjs/builders'
-import { ApplicationCommandTypes } from 'discord.js/typings/enums'
 
-export interface CommandBuilderOutput
-  extends ReturnType<SlashCommandBuilder['toJSON']> {
-  type?: ApplicationCommandTypes
-}
+export type CommandBuilderOutput = ReturnType<SlashCommandBuilder['toJSON']>
