88dbf9072860d4e208bf2e9ae1150cc913a1b4af
Felix <jonfelixrico@gmail.com>
2021-09-18 00:58:23 +0800

    Integrate announcing of the highest roll

--- src/interactions/announcers/roll-announcer/roll-announcer.service.ts
+++ src/interactions/announcers/roll-announcer/roll-announcer.service.ts
@@ -1,4 +1,4 @@
-import { EventsHandler, IEventHandler } from '@nestjs/cqrs'
+import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs'
 import { MessageEmbedOptions } from 'discord.js'
 import { InteractionCache } from 'src/interactions/providers/interaction-cache.class'
 import { RollPresentationSerializerService } from 'src/interactions/services/roll-presentation-serializer/roll-presentation-serializer.service'
@@ -6,7 +6,12 @@ import { RollDbEntity } from 'src/read-model/entities/roll.db-entity'
 import { ReadModelSyncedEvent } from 'src/read-model/read-model-synced.event'
 import { IRollCreatedEvent } from 'src/write-model/types/roll-created-event.interface'
 import { Connection } from 'typeorm'
-import { PrizeTier, PrizeTierLabels } from 'src/utils/prize-eval'
+import { PrizeTierLabels } from 'src/utils/prize-eval'
+import {
+  HighestRollQuery,
+  HighestRollQueryOutput,
+} from 'src/query/highest-roll.query'
+import { ChannelCutoffTimestampQuery } from 'src/query/channel-cutoff-timestamp.query'
 
 function generateDudRollResponse(roll: RollDbEntity): MessageEmbedOptions {
   return {
@@ -15,11 +20,18 @@ function generateDudRollResponse(roll: RollDbEntity): MessageEmbedOptions {
 }
 
 function generateWinningRollResponse(
-  roll: RollDbEntity,
-  rank: PrizeTier
+  newRoll: RollDbEntity,
+  highest: HighestRollQueryOutput
 ): MessageEmbedOptions {
+  const rankText = PrizeTierLabels[newRoll.prizeRank]
+  if (newRoll.rollId !== highest?.rollId) {
+    return {
+      description: `<@${newRoll.rollOwner}> has rolled **${rankText}**.`,
+    }
+  }
+
   return {
-    description: `<@${roll.rollOwner}> has rolled **${PrizeTierLabels[rank]}**`,
+    description: `<@${newRoll.rollOwner}> has rolled **${rankText}**. They now have the highest roll in <#${newRoll.channelId}>!`,
   }
 }
 @EventsHandler(ReadModelSyncedEvent)
@@ -29,7 +41,8 @@ export class RollAnnouncerService
   constructor(
     private interactions: InteractionCache,
     private typeorm: Connection,
-    private serializer: RollPresentationSerializerService
+    private serializer: RollPresentationSerializerService,
+    private queryBus: QueryBus
   ) {}
 
   async handle({ payload: event }: ReadModelSyncedEvent<IRollCreatedEvent>) {
@@ -51,16 +64,31 @@ export class RollAnnouncerService
 
     interactions.del(rollId)
 
+    // TODO use a query for this instead of directly using typeorm
     const roll = await typeorm.getRepository(RollDbEntity).findOne({ rollId })
     if (!roll) {
       // TODO add warn logging
       return
     }
 
+    const { channelId, guildId } = interaction
+
+    const cutoff = await this.queryBus.execute(
+      new ChannelCutoffTimestampQuery({ channelId, guildId })
+    )
+
+    const highestRoll: HighestRollQueryOutput = await this.queryBus.execute(
+      new HighestRollQuery({
+        channelId,
+        guildId,
+        startingFrom: cutoff,
+      })
+    )
+
     const responseFragment =
       roll.prizeRank === null
         ? generateDudRollResponse(roll)
-        : generateWinningRollResponse(roll, roll.prizeRank)
+        : generateWinningRollResponse(roll, highestRoll)
 
     const embed: MessageEmbedOptions = {
       footer: {
