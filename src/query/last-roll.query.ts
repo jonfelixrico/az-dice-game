348c4ded83c1edc03a0eafa304a28a024580a7f7
Felix <jonfelixrico@gmail.com>
2021-09-18 15:58:12 +0800

    Add comment to LastRollQuery

--- src/query/last-roll.query.ts
+++ src/query/last-roll.query.ts
@@ -3,6 +3,10 @@ import { ChannelHistoryQueryParams, ChannelRoll } from './commons.interfaces'
 
 export type LastRollQueryInput = ChannelHistoryQueryParams
 export type LastRollQueryOutput = ChannelRoll
+
+/**
+ * Queries for the last **undeleted** roll in a given channel.
+ */
 export class LastRollQuery implements IQuery {
   constructor(readonly input: LastRollQueryInput) {}
 }
