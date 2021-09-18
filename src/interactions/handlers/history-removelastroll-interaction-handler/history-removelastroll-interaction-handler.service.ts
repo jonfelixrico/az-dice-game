a4042b8aba94e9d4011aa47123dedab012cc39ac
Felix <jonfelixrico@gmail.com>
2021-09-18 16:06:43 +0800

    Implement handler for /history removelastroll

--- src/interactions/handlers/history-removelastroll-interaction-handler/history-removelastroll-interaction-handler.service.ts
+++ src/interactions/handlers/history-removelastroll-interaction-handler/history-removelastroll-interaction-handler.service.ts
@@ -1,4 +1,49 @@
-import { Injectable } from '@nestjs/common'
+import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs'
+import { pick } from 'lodash'
+import { InteractionCreatedEvent } from 'src/interactions/services/interaction-events-relay/interaction-created.event'
+import { LastRollQuery, LastRollQueryOutput } from 'src/query/last-roll.query'
+import { EsdbHelperService } from 'src/write-model/services/esdb-helper/esdb-helper.service'
+import { IRollRemovedEvent } from 'src/write-model/types/roll-removed-event.interface'
 
-@Injectable()
-export class HistoryRemovelastrollInteractionHandlerService {}
+@EventsHandler(InteractionCreatedEvent)
+export class HistoryRemovelastrollInteractionHandlerService
+  implements IEventHandler<InteractionCreatedEvent>
+{
+  constructor(private helper: EsdbHelperService, private queryBus: QueryBus) {}
+
+  async handle({ interaction }: InteractionCreatedEvent) {
+    if (
+      !interaction.isCommand() ||
+      interaction.commandName !== 'history' ||
+      interaction.options.getSubcommand() !== 'removelastroll'
+    ) {
+      return
+    }
+
+    await interaction.deferReply({ ephemeral: true })
+
+    const channelParams = pick(interaction, 'guildId', 'channelId')
+    const lastRoll: LastRollQueryOutput = await this.queryBus.execute(
+      new LastRollQuery(channelParams)
+    )
+
+    if (!lastRoll) {
+      await interaction.editReply('No rolls have been made yet.')
+      return
+    }
+
+    await this.helper.pushEvent<IRollRemovedEvent>({
+      type: 'ROLL_REMOVED',
+      payload: {
+        ...channelParams,
+        rollId: lastRoll.rollId,
+        timestamp: new Date(),
+        userId: interaction.user.id,
+      },
+    })
+
+    await interaction.editReply(
+      `Roll ${lastRoll.rollId} was successfully deleted.`
+    )
+  }
+}
