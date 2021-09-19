80ef466a91b94c3bd6c2f62c1fb96432b07924a0
Felix <jonfelixrico@gmail.com>
2021-09-19 23:09:28 +0800

    Apply fixes to the quip service

--- src/interactions/services/quip-generator/quip-generator.service.ts
+++ src/interactions/services/quip-generator/quip-generator.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common'
-import { chain, random, range } from 'lodash'
-import quips from 'src/../quips.json'
+import { Injectable, Logger } from '@nestjs/common'
+import { chain, random, range, truncate } from 'lodash'
+import { sprintf } from 'sprintf-js'
+import quipsJson = require('./quips.json')
 import { PrizeTier } from 'src/utils/prize-eval'
 
 interface QuipEntry {
@@ -13,7 +14,7 @@ export type Quip = Omit<QuipEntry, 'prizeTiers'>
 
 const ALL_TIERS = range(1, 7) // generate numbers 1-6
 
-const GROUPED_QUIPS = chain(quips as QuipEntry[])
+const GROUPED_QUIPS = chain(quipsJson?.quips as QuipEntry[])
   .map((entry) => {
     const { prizeTiers } = entry
     const tierArray = prizeTiers && prizeTiers.length ? prizeTiers : ALL_TIERS
@@ -32,16 +33,35 @@ const GROUPED_QUIPS = chain(quips as QuipEntry[])
 
 @Injectable()
 export class QuipGeneratorService {
-  getQuipForRank(rank: PrizeTier | null): QuipEntry {
+  constructor(private logger: Logger) {}
+
+  getQuipForRank(rank: PrizeTier | null): Quip {
     rank = rank ?? 0
+    const { logger } = this
 
     const quipGroup = GROUPED_QUIPS[rank]
 
     if (!quipGroup || !quipGroup.length) {
+      logger.debug(
+        sprintf("Can't find any quips for rank %d", rank),
+        QuipGeneratorService.name
+      )
       return
     }
 
     const index = random(0, quipGroup.length - 1)
-    return quipGroup[index]
+    const quip = quipGroup[index]
+
+    logger.debug(
+      sprintf(
+        'Using quip with text "%s" and image "%s" for rank %d',
+        truncate(quip.text, { length: 25 }),
+        truncate(quip.image, { length: 25 }),
+        rank
+      ),
+      QuipGeneratorService.name
+    )
+
+    return quip
   }
 }
