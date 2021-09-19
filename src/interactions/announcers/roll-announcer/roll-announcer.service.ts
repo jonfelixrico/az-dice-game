cd499982059e4b73fed23b07207f64e4e2432c6d
Felix <jonfelixrico@gmail.com>
2021-09-19 23:09:52 +0800

    Integrate the quip service into RollAnnouncerService

--- src/interactions/announcers/roll-announcer/roll-announcer.service.ts
+++ src/interactions/announcers/roll-announcer/roll-announcer.service.ts
@@ -12,6 +12,7 @@ import {
   HighestRollQueryOutput,
 } from 'src/query/highest-roll.query'
 import { ChannelCutoffTimestampQuery } from 'src/query/channel-cutoff-timestamp.query'
+import { QuipGeneratorService } from 'src/interactions/services/quip-generator/quip-generator.service'
 
 function generateDudRollResponse(roll: RollDbEntity): MessageEmbedOptions {
   return {
@@ -42,7 +43,8 @@ export class RollAnnouncerService
     private interactions: InteractionCache,
     private typeorm: Connection,
     private serializer: RollPresentationSerializerService,
-    private queryBus: QueryBus
+    private queryBus: QueryBus,
+    private quipGenerator: QuipGeneratorService
   ) {}
 
   async handle({
@@ -97,6 +99,22 @@ export class RollAnnouncerService
         text: roll.rollId,
       },
       ...responseFragment,
+    }
+
+    const quip = this.quipGenerator.getQuipForRank(roll.prizeRank)
+    if (quip?.image) {
+      embed.image = {
+        url: quip.image,
+      }
+    }
+
+    if (quip?.text) {
+      embed.fields = [
+        {
+          name: '\u200B',
+          value: quip.text,
+        },
+      ]
     }
 
     await interaction.editReply({
