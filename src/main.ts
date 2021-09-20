46b111c4666fce9cf1fd71ade94a10faad344daf
Felix <jonfelixrico@gmail.com>
2021-09-21 02:10:31 +0800

    Set default zone to Asia/Manila

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core'
+import { Settings } from 'luxon'
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston'
 import { AppModule } from './app.module'
+
+Settings.defaultZone = 'Asia/Manila'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
