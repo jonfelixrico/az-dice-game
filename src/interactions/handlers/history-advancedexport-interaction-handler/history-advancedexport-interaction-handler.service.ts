62a48ca7b9d247c6322888e5b7592907487e9844
Felix <jonfelixrico@gmail.com>
2021-09-21 01:22:06 +0800

    Improve presentation in AdvancedExport

--- src/interactions/handlers/history-advancedexport-interaction-handler/history-advancedexport-interaction-handler.service.ts
+++ src/interactions/handlers/history-advancedexport-interaction-handler/history-advancedexport-interaction-handler.service.ts
@@ -1,5 +1,5 @@
 import { EventsHandler, IEventHandler, QueryBus } from '@nestjs/cqrs'
-import { TextChannel } from 'discord.js'
+import { MessageEmbedOptions, TextChannel } from 'discord.js'
 import { pick } from 'lodash'
 import { DateTime } from 'luxon'
 import { AdvancedHistoryExporterService } from 'src/interactions/services/advanced-history-exporter/advanced-history-exporter.service'
@@ -8,10 +8,29 @@ import {
   parsePrizeLimits,
   PRIZE_LIMITS_REGEXP,
 } from 'src/interactions/utils/roll-breakdown.utils'
+import { ChannelCutoffTimestampQuery } from 'src/query/channel-cutoff-timestamp.query'
 import {
+  PrizeLimits,
   RollBreakdownQuery,
   RollBreakdownQueryOutput,
 } from 'src/query/roll-breakdown.query'
+import { PrizeTier, PrizeTierLabels } from 'src/utils/prize-eval'
+
+const { IT_SIU, DI_KI, SAM_HONG, SI_CHIN, TWI_THENG, CHIONG_GUAN } = PrizeTier
+const RANK_DISPLAY_SEQUENCE = [
+  IT_SIU,
+  DI_KI,
+  SAM_HONG,
+  SI_CHIN,
+  TWI_THENG,
+  CHIONG_GUAN,
+]
+
+function displayLimits(limits: PrizeLimits): string {
+  return RANK_DISPLAY_SEQUENCE.map(
+    (rankCode) => `**${limits[rankCode] ?? 0}** ${PrizeTierLabels[rankCode]}`
+  ).join('\n')
+}
 
 @EventsHandler(InteractionCreatedEvent)
 export class HistoryAdvancedexportInteractionHandlerService
@@ -44,11 +63,11 @@ export class HistoryAdvancedexportInteractionHandlerService
 
     await interaction.deferReply()
 
-    const input = pick(interaction, ['channelId', 'guildId'])
+    const channelInfo = pick(interaction, ['channelId', 'guildId'])
 
     const breakdown: RollBreakdownQueryOutput = await this.queryBus.execute(
       new RollBreakdownQuery({
-        ...input,
+        ...channelInfo,
         prizeLimits,
       })
     )
@@ -63,7 +82,32 @@ export class HistoryAdvancedexportInteractionHandlerService
       .toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
       .replace(/ /gi, '-')
 
+    const end = DateTime.now().toLocaleString(
+      DateTime.DATETIME_MED_WITH_SECONDS
+    )
+    const start = DateTime.fromJSDate(
+      await this.queryBus.execute(
+        new ChannelCutoffTimestampQuery({
+          ...channelInfo,
+          useOriginDateIfNotFound: true,
+        })
+      )
+    ).toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS)
+
+    const embed: MessageEmbedOptions = {
+      author: {
+        name: 'Export History',
+      },
+      description: [
+        `Exported breakdown of rolls from **${start}** to **${end}**`,
+        '',
+        '**Prize limits**',
+        displayLimits(prizeLimits),
+      ].join('\n'),
+    }
+
     await interaction.editReply({
+      embeds: [embed],
       files: [
         {
           attachment: xlsxBuffer,
